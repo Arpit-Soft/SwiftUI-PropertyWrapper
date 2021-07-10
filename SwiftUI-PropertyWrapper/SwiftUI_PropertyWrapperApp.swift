f65d3cb3994793b9f70f8ead37d5546c37ee68d6
Arpit Dixit <arpitd@dewsolutions.in>
2021-07-10 11:53:08 +0530

    Initial Commit

---
+++ SwiftUI-PropertyWrapper/SwiftUI_PropertyWrapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PropertyWrapperApp.swift
+//  SwiftUI-PropertyWrapper
+//
+//  Created by Arpit Dixit on 10/07/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PropertyWrapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
